9d59016e034eb16cad4d3f0990fccb579a62f0c0
Mariana7IT <153327568+Mariana7IT@users.noreply.github.com>
2024-07-29 20:49:35 +0300

    MoviesPage

--- src/pages/MoviesPage/MoviesPage.jsx
+++ src/pages/MoviesPage/MoviesPage.jsx
@@ -1,12 +1,30 @@
-import React from 'react';
-import { fetchTrendingMovies } from "/src/services/api.js";
+import  { useEffect, useState } from "react";
+import api from "/src/services/api.js";
+import MovieList from "/src/components/MovieList/MovieList";
 
 const MoviesPage = () => {
+  const [query, setQuery] = useState("");
+  const [movies, setMovies] = useState([]);
+
+  const handleSearch = async () => {
+    if (query) {
+      const movies = await searchMovies(query);
+      setMovies(movies);
+    }
+  };
+
   return (
     <div>
-      
+      <h1>Search Movies</h1>
+      <input
+        type="text"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+      />
+      <button onClick={handleSearch}>Search</button>
+      <MovieList movies={movies} />
     </div>
-  )
-}
+  );
+};
 
 export default MoviesPage;
