d3199931571306dda0c3a6eb90e324b01153008d
Mariana7IT <153327568+Mariana7IT@users.noreply.github.com>
2024-07-30 22:33:10 +0300

    Reviews

--- src/components/MovieReviews/MovieReviews.jsx
+++ src/components/MovieReviews/MovieReviews.jsx
@@ -1,37 +1,36 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { fetchTrendingMovies } from "/src/services/api.js";
+import { fetchMovieReviewById } from "/src/services/api.js";
 import s from "./MovieReviews.module.css";
 
 const MovieReviews = () => {
   const { movieId } = useParams();
   const [reviews, setReviews] = useState([]);
   const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(true); 
 
   useEffect(() => {
     const fetchReviews = async () => {
       try {
-        const data = await fetchReviewsById(movieId);
-        setReviews(data.results);
+        const data = await fetchMovieReviewById(movieId); 
       } catch (error) {
         setError(`Error fetching reviews: ${error.message}`);
+      } finally {
+        setIsLoading(false); 
       }
     };
     if (movieId) fetchReviews();
   }, [movieId]);
 
-  if (error) {
-    return <div>{error}</div>;
-  }
+  if (isLoading) return <div>Loading reviews...</div>; 
+  if (error) return <div>{error}</div>;
 
-  if (reviews.length === 0) {
-    return <div>No reviews found</div>;
-  }
+  if (reviews.length === 0) return <div>No reviews found</div>;
 
   return (
-    <ul>
+    <ul className={s.reviewList}>
       {reviews.map((review) => (
-        <li key={review.id}>
+        <li key={review.id} className={s.reviewItem}>
           <h3>{review.author}</h3>
           <p>{review.content}</p>
         </li>
